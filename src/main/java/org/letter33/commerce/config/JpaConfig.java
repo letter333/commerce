c94d5243b5e591e56ac8da6a63ed0cec929cc279
letter333 <68866597+letter333@users.noreply.github.com>
2026-01-04 18:58:40 +0900

    Revert "feature/user-login"

--- src/main/java/org/letter33/commerce/config/JpaConfig.java
+++
@@ -1,9 +0,0 @@
-package org.letter33.commerce.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-@Configuration
-@EnableJpaAuditing
-public class JpaConfig {
-}
